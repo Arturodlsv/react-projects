9ea97ec83ac88779ae550e1428735bb050a169c3
svol-enterprise <svolenterprise@gmail.com>
2023-09-11 15:48:43 -0400

    Base Project: Shofu Backend

--- shoppify/server/src/app.ts
+++ shoppify/server/src/app.ts
@@ -11,5 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT} 😎😉`);
-})
+  console.log(`Server is running on port ${process.env.PORT} 😎😉`);
+});
