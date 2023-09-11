3efd0dae9ba4597154275a71143cc0d3ac80ad8f
svol-enterprise <svolenterprise@gmail.com>
2023-09-11 13:44:26 -0400

    Base Projec: Shofu

---
+++ shoppify/client/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  ignorePatters: [
+    './postcss.config.js'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
+}
