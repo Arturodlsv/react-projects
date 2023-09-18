93683e4da2e94d4cee7857676f96e7c3e2288796
svol-enterprise <svolenterprise@gmail.com>
2023-09-18 11:18:55 -0400

    ItemDetails route created

--- shoppify/client/src/routes/PageRoutes.tsx
+++ shoppify/client/src/routes/PageRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../view/HomeView'
 import SearchView from '../view/SearchView'
+import ItemsDetails from '../view/ItemsDetails'
 
 const PageRoutes = () => {
   return (
@@ -8,6 +9,8 @@ const PageRoutes = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/items' element={<SearchView />} />
+        <Route path='*' element={<Home />} />
+        <Route path='/items/:id' element={<ItemsDetails />} />
       </Routes>
     </Router>
   )
